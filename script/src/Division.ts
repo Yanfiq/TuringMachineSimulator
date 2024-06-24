5189a79b42a2a5d08d44d5ebc3614c4cc119c1e1
Muhammad Ryan Fikri Fakhrezi <113404595+Yanfiq@users.noreply.github.com>
2024-06-24 14:18:42 +0700

    implementing negative division

--- script/src/Division.ts
+++ script/src/Division.ts
@@ -19,22 +19,49 @@ export class Division {
         const q4: State = new State('q4', false);
         const q5: State = new State('q5', false);
         const q6: State = new State('q6', true);
+        const q7: State = new State('q7', false);
+        const q8: State = new State('q8', false);
+        const q9: State = new State('q9', false);
+        const q10: State = new State('q9', false);
             
-        q0.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'RSS', nextState: q0});
-        q0.addTransition({inputSymbol: '1BB', writeSymbol: '1BB', direction: 'RSS', nextState: q1});
+        q0.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'RSS', nextState: q7});
+        q0.addTransition({inputSymbol: '1BB', writeSymbol: '1B1', direction: 'RSS', nextState: q7});
+
+        q7.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'RSS', nextState: q7});
+        q7.addTransition({inputSymbol: '0B1', writeSymbol: '0B1', direction: 'RSS', nextState: q7});
+        q7.addTransition({inputSymbol: '1BB', writeSymbol: '1BB', direction: 'RSS', nextState: q8});
+        q7.addTransition({inputSymbol: '1B1', writeSymbol: '1B1', direction: 'RSS', nextState: q8});
+
+        q8.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'SSS', nextState: q1});
+        q8.addTransition({inputSymbol: '0B1', writeSymbol: '0B1', direction: 'SSR', nextState: q1});
+        q8.addTransition({inputSymbol: '1B1', writeSymbol: '1BB', direction: 'RSR', nextState: q1});
+        q8.addTransition({inputSymbol: '1BB', writeSymbol: '1B1', direction: 'RSR', nextState: q1});
+
         q1.addTransition({inputSymbol: '0BB', writeSymbol: '00B', direction: 'RRS', nextState: q1});
+        q1.addTransition({inputSymbol: '0B1', writeSymbol: '001', direction: 'RRS', nextState: q1});
         q1.addTransition({inputSymbol: 'BBB', writeSymbol: 'BBB', direction: 'LLS', nextState: q2});
+        q1.addTransition({inputSymbol: 'BB1', writeSymbol: 'BBB', direction: 'LLS', nextState: q2});
+        
         q2.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'LSS', nextState: q2});
         q2.addTransition({inputSymbol: '00B', writeSymbol: '00B', direction: 'LLS', nextState: q2});
-        q2.addTransition({inputSymbol: '10B', writeSymbol: '10B', direction: 'LLS', nextState: q2});
-        q2.addTransition({inputSymbol: '1BB', writeSymbol: '1BB', direction: 'LSS', nextState: q2});
+        q2.addTransition({inputSymbol: '1BB', writeSymbol: '1BB', direction: 'LSS', nextState: q9});
         q2.addTransition({inputSymbol: 'BBB', writeSymbol: 'BBB', direction: 'RRS', nextState: q3});
+
+        q9.addTransition({inputSymbol: '1BB', writeSymbol: '1BB', direction: 'LSS', nextState: q9});
+        q9.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'LSS', nextState: q2});
+        q9.addTransition({inputSymbol: 'BBB', writeSymbol: 'BBB', direction: 'RSS', nextState: q10});
+
+        q10.addTransition({inputSymbol: '1BB', writeSymbol: '1BB', direction: 'RRS', nextState: q3});
+        q10.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'SRS', nextState: q3});
+
         q3.addTransition({inputSymbol: '00B', writeSymbol: 'X0B', direction: 'RRS', nextState: q3});
         q3.addTransition({inputSymbol: '0BB', writeSymbol: '0B0', direction: 'SLR', nextState: q4});
         q3.addTransition({inputSymbol: '1BB', writeSymbol: '1B0', direction: 'SSS', nextState: q6});    
         q3.addTransition({inputSymbol: '10B', writeSymbol: '10B', direction: 'SSS', nextState: q6});   
+        
         q4.addTransition({inputSymbol: '00B', writeSymbol: '00B', direction: 'SLS', nextState: q4});
         q4.addTransition({inputSymbol: '0BB', writeSymbol: '0BB', direction: 'SRS', nextState: q5});
+        
         q5.addTransition({inputSymbol: '00B', writeSymbol: 'X0B', direction: 'RRS', nextState: q3});
 
         this.diagram = new Diagram([q0, q1, q2, q3, q4, q5], q0);
@@ -76,10 +103,14 @@ export class Division {
                     this.result = 0;
                     let tapeElement = this.tape_result.getHtmlElement()
                     let tapeChild = tapeElement.childNodes
+                    let isNegative = false;
                     for(let i=0; i<tapeChild.length; i++){
+                        if((<HTMLElement> tapeChild[i]).innerText == "1"){
+                            isNegative = true;
+                        }
                         this.result += (<HTMLElement> tapeChild[i]).innerText == "0" ? 1:0;
                     }
-                    console.log(this.result);
+                    isNegative ? this.result*=-1 : '';
                 }
                 this.stop();
             }
